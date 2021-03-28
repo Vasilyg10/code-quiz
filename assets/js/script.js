fbe110a6df91b06071299356698392f4fccd6925
Vasilyg10 <zvgianna@gmail.com>
2021-03-28 17:25:55 -0500

    added first question to array with timer starting on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,62 @@
+// starting values for high score and timer
+
+
+const highScore = 0;
+
+let shuffledQuestions, questionIndex
+
+let quizEl = document.getElementById("questions")
+let answerOptionsEl = document.getElementById("answer-options")
+
+let questions = [
+    {
+        question: "What color is the sky??",
+        answers: [
+            { text: "Blue", correct: true },
+            { text: "Red", correct: false }
+        ]
+    }
+]
+
+
 // start the game
 let startQuiz = function() {
-    console.log("The game has started!");
     firstClickEl.classList.add("hidden");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    questionIndex = 0;
     startWordsEl.classList.add("hidden");
+    startTitleEl.classList.add("hidden");
     questionsEl.classList.remove("hidden");
-    timerStartEl.classList.remove("hidden");
+
+    let count = 60;
+    let timer = setInterval(function() {
+        document.getElementById("timer").innerHTML = "Timer: " + count;
+        count--;
+        if (count <= 0) {
+            clearInterval(timer);
+            document.getElementById("timer").innerHTML = "Time is up!"
+        }
+    }, 1000);
+    nextQuestion();
 }
 
 // move to the next question
-let nextQuestion = function() {
+function nextQuestion() {
+    showQuestion(shuffledQuestions[questionIndex])
+}
 
+function showQuestion(question) {
+    quizEl.innerText = question.question
+    question.answers.forEach(answer => {
+        let button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerOptionsEl.appendChild(button)
+    })
 }
 
 // pick an answer
@@ -17,9 +64,14 @@ let selectAnswer = function() {
 
 }
 
+function resetQuestion() {
+    nextButton.classList.add("hidden")
+}
+
 let firstClickEl = document.getElementById("first-click");
 let questionsEl = document.getElementById("questions-container");
 let startWordsEl = document.getElementById("start-words");
 let timerStartEl = document.getElementById("timer");
+let startTitleEl = document.getElementById("start-title");
 
 firstClickEl.addEventListener("click", startQuiz);
